b4ba3771526ba341118ca02fdc3d0b5e356520d1
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2023-03-30 22:04:24 +0200

    ci: Update nonfree setup

--- target/sim/vsim/start.cheshire_soc.tcl
+++ target/sim/vsim/start.cheshire_soc.tcl
@@ -9,14 +9,13 @@
 
 set TESTBENCH tb_cheshire_soc
 
-set flags "-permissive -suppress 3009 -suppress 8386"
+set flags "-permissive -suppress 3009 -suppress 8386 -error 7"
 
 set pargs ""
-if ([info exists BOOTMODE]) { append pargs "+BOOTMODE=${BOOTMODE} " }
-if ([info exists PRELMODE]) { append pargs "+PRELMODE=${PRELMODE} " }
-if ([info exists TESTMODE]) { append pargs "+TESTMODE=${TESTMODE} " }
-if ([info exists BINARY])   { append pargs "+BINARY=${BINARY} " }
-if ([info exists IMAGE])    { append pargs "+IMAGE=${IMAGE} " }
+if {[info exists BOOTMODE]} { append pargs "+BOOTMODE=${BOOTMODE} " }
+if {[info exists PRELMODE]} { append pargs "+PRELMODE=${PRELMODE} " }
+if {[info exists BINARY]}   { append pargs "+BINARY=${BINARY} " }
+if {[info exists IMAGE]}    { append pargs "+IMAGE=${IMAGE} " }
 
 eval "vsim -c ${TESTBENCH} -t 1ps -voptargs=+acc" ${pargs} ${flags}
 
